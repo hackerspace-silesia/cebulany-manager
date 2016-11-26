f167c377673ab0209d22048497144ca5df79aadd
Firemark <marpiechula@gmail.com>
2016-11-26 19:02:35 +0100

    add years

--- cebulany/resources/report.py
+++ cebulany/resources/report.py
@@ -40,15 +40,13 @@ class ReportMonth(object):
             sql_func.count(distinct(PaidMonth.member_id)),
         ).join(
             PaidMonth.transaction
-        ).filter(year_field == self.year, month_field == self.month)
+        )
 
-        paids_value, member_count = query.first()
+        paids_value, member_count = self.filterize_query(query).first()
         self.rows.append(Money(u'SKŁADKI', paids_value))
         self.rows.append(Row(u'WPŁACIŁO', member_count))
-        if member_count > 0:
-            avg_value = paids_value / member_count
-            self.rows.append(Money(u'ŚREDNIA SKŁADEK', avg_value))
         return paids_value
+
 
     def compute_bills(self):
         query = db.session.query(
@@ -56,10 +54,8 @@ class ReportMonth(object):
             sql_func.sum(Bill.cost),
         ).join(
             Bill.transaction
-        ).filter(
-            year_field == self.year,
-            month_field == self.month,
         ).group_by(sql_func.upper(Bill.name))
+        query = self.filterize_query(query)
 
         total = Decimal('0.00')
         for name, cost in query.all():
@@ -75,9 +71,9 @@ class ReportMonth(object):
             sql_func.sum(Donation.cost),
         ).join(
             Donation.transaction
-        ).filter(year_field == self.year, month_field == self.month)
+        )
 
-        donations_value = query.scalar() or 0
+        donations_value = self.filterize_query(query).scalar() or 0
         if donations_value > 0:
             self.rows.append(Money(u'DAROWIZNY', donations_value))
         return donations_value
@@ -88,10 +84,8 @@ class ReportMonth(object):
             sql_func.sum(Other.cost),
         ).join(
             Other.transaction
-        ).filter(
-            year_field == self.year,
-            month_field == self.month,
         ).group_by(sql_func.upper(Other.name))
+        query = self.filterize_query(query)
 
         total = Decimal(0)
         for name, cost in query.all():
@@ -99,6 +93,17 @@ class ReportMonth(object):
             self.rows.append(Money(name, cost))
 
         return total
+
+    def filterize_query(self, query):
+        if self.month is None:
+            return query.filter(
+                year_field == self.year,
+            )
+        else:
+            return query.filter(
+                year_field == self.year,
+                month_field == self.month,
+            )
 
 
 class Row(object):
@@ -139,6 +144,18 @@ def basic():
     months = [
         ReportMonth(year, month, total)
         for year, month, total in query_dates.all()
+    ]
+    query_dates = db.session.query(
+        year_field,
+        sql_func.sum(Transaction.cost),
+    ).group_by(
+        year_field
+    ).order_by(
+        year_field
+    )
+    months += [
+        ReportMonth(year, None, total)
+        for year, total in query_dates.all()
     ]
 
     return render_template('report.html', months=months)
