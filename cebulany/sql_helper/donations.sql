72a759a4657e33083163efb17dafca926c7a0dfe
Firemark <marpiechula@gmail.com>
2018-04-14 12:35:40 +0200

    add new sql helpers

--- cebulany/sql_helper/donations.sql
+++ cebulany/sql_helper/donations.sql
@@ -1,4 +1,7 @@
-select Sum(donation.cost), strftime('%Y', "date") from donation
+select 
+	Sum(donation.cost) as "sum",
+	strftime('%Y', "date") as "year"
+from donation
 inner join "transaction" on donation.transaction_id = "transaction".id
 group by strftime('%Y', "transaction"."date")
 order by strftime('%Y', "transaction"."date")
--- cebulany/sql_helper/others_with_neg.sql
+++ cebulany/sql_helper/others_with_neg.sql
@@ -4,5 +4,5 @@ select
 	strftime('%Y', "transaction"."date") as "year"
 from other
 inner join "transaction" on other.transaction_id = "transaction".id
-group by other.name, strftime('%Y', "transaction"."date")
-order by strftime('%Y', "transaction"."date"), other.name
+group by other.name, other.cost > 0, strftime('%Y', "transaction"."date")
+order by strftime('%Y', "transaction"."date"), other.name, other.cost > 0
