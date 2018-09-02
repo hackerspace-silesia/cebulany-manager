fbdd30b4ef3f699f83e3be0eb5ceb32a6e3b19b7
Firemark <marpiechula@gmail.com>
2018-09-02 13:19:57 +0200

    cleanups

--- spa/src/components/MenuPanel.vue
+++ spa/src/components/MenuPanel.vue
@@ -3,10 +3,9 @@
     b-nav-toggle(target="nav_collapse")
     b-navbar-brand(href="#/") Cebulany Manager
     b-collapse(is-nav id="nav_collapse")
-      b-nav(is-nav-bar)
+      b-navbar-nav
         b-nav-item(href="#/") Przelewy
         b-nav-item(href="#/members") Członkowie
-        b-nav-item(href="#/bills") Rachunki
-        b-nav-item(href="#/donations") Darowizny
-        b-nav-item(href="#/others") Inne płatności
+        b-nav-item(href="#/budgets") Budżety
+        b-nav-item(href="#/payment-types") Typy płatności
 </template>
--- spa/src/router/index.js
+++ spa/src/router/index.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Transactions from '@/components/transactions/Transactions'
-import GenericTypes from '@/components/genericTypes/GenericTypes'
 import Members from '@/components/members/Members'
-
-import BillService from '@/services/bill'
-import DonationService from '@/services/donation'
-import OtherService from '@/services/other'
 
 Vue.use(Router);
 
@@ -21,24 +16,6 @@ export default new Router({
       path: '/members',
       name: 'Members',
       component: Members
-    },
-    {
-      path: '/bills',
-      name: 'bills',
-      component: GenericTypes,
-      props: {'service': BillService, 'title': 'Rachunki'}
-    },
-    {
-      path: '/donations',
-      name: 'donations',
-      component: GenericTypes,
-      props: {'service': DonationService, 'title': 'Darowizny'}
-    },
-    {
-      path: '/others',
-      name: 'others',
-      component: GenericTypes,
-      props: {'service': OtherService, 'title': 'Inne płatności'}
     }
   ]
 })
