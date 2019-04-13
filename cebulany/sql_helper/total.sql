5f57cd7a186151253c1fcbf12ba2290693cf593b
Firemark <marpiechula@gmail.com>
2019-04-13 13:22:00 +0200

    fix sql helper

--- cebulany/sql_helper/total.sql
+++ cebulany/sql_helper/total.sql
@@ -1,5 +1,6 @@
-SELECT b.name, sum(p.cost) from budget b
+SELECT b.name budget_name, pt.name type_name, round(sum(p.cost), 2) total from budget b
 join payment p on b.id = p.budget_id
+join paymenttype pt on pt.id = p.payment_type_id
 join "transaction" t on p.transaction_id = t.id
 where strftime('%Y', t."date") == '2018'
-group by b.id
+group by b.id, pt.id
