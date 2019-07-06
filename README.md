b21620ec97ee500038d326582ec3fae30738d3c4
Marek Piechula <m.piechula@clearcode.cc>
2019-07-06 18:28:31 +0200

    small changes + readme

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ python setup.py install
 cd spa/
 yarn run build
 ```
+
+# Init 
+
+```
+alembic upgrade head
+python create_mock.py > mock.csv
+./uwsgi-me.sh
+# login (socek, socek) and upload mock.csv in transaction view
+```
+
+# Run?
+
+```
+./uwsgi-me.sh
+```
--- create_mock.py
+++ create_mock.py
@@ -3,8 +3,8 @@
 from datetime import date, timedelta
 from random import randint, choice, random
 from decimal import Decimal
-start_date = date(2015, 1, 1)
-money = Decimal('2000.00')
+from string import ascii_uppercase
+start_date = date.today() - timedelta(days=30)
 
 firsts = [
     u'MAREK', u'DAREK', u'HENRYK', u'MICHAŁ', u'ZYGFRYD', u'PAWEŁ', u'STANISŁAW',
@@ -17,57 +17,23 @@ lasts = [
 
 names = [u'{} {}'.format(choice(firsts), choice(lasts)) for _ in range(30)]
 
-print('Data operacji,Opis,Kwota transakcji,Saldo po operacji'.encode('windows-1250'))
-
-for i in range(1000):
-    start_date += timedelta(days=randint(0, 2))
-    dt = start_date.strftime('%Y-%m-%d')
-    rev_dt = start_date.strftime('%d-%m-%Y')
+for i in range(100):
+    start_date += timedelta(days=randint(0, 5))
+    dt = start_date.strftime('%d-%m-%Y')
     iban = ''.join(str(randint(0,9)) for _ in range(26))
+    t_id = ''.join(choice(ascii_uppercase) for _ in range(50))
+    name = choice(names)
+    addr = f'{name} WOJSKA CZESKIEGO 43 BRNO'
     x = random()
-    if x > .2:
-        desc = (
-            u'PRZELEW UZNANIOWY (NADANO {dt})'
-            u' SKŁADKA CZŁONKOWSKA ZA MIESIĄC {month}'
-            u'  {name}'
-            u'  WOJSKA CZESKIEGO 43 BRNO {iban}'
-        ).format(
-            dt=rev_dt,
-            month=start_date.strftime('%m-%Y'),
-            iban=iban,
-            name=choice(names),
-        )
+    if x > .3:
+        month = start_date.strftime('%m-%Y')
+        desc = f'SKŁADKA CZŁONKOWSKA ZA MIESIĄC {month}'
         cost = Decimal(choice([16,32,64,128]))
     elif x > .1:
-        desc = (
-            u'PRZELEW OBCIĄŻENIOWY (NADANO {dt})'
-            u' COS DO PLACENIA'
-            u'  {name}'
-            u'  WOJSKA CZESKIEGO 43 BRNO {iban}'
-        ).format(
-            dt=rev_dt,
-            iban=iban,
-            name=choice(['UPC', 'TAURON', 'LOKAL'])
-        )
+        desc = 'COS DO PLACENIA'
         cost = -Decimal(randint(256, 1024))
     else:
-        desc = (
-            u'PRZELEW UZNANIOWY (NADANO {dt})'
-            u' DAROWIZNA'
-            u'  {name}'
-            u'  WOJSKA CZESKIEGO 43 BRNO {iban}'
-        ).format(
-            dt=rev_dt,
-            iban=iban,
-            name=choice(names),
-        )
+        desc = 'DAROWIZNA'
+        cost = -Decimal(randint(256, 1024))
         cost = Decimal(randint(256, 1024))
-    money -= cost
-    ss = u'{dt},{desc},{cost},{money}'.format(
-        dt=dt,
-        desc=desc,
-        cost=cost,
-        money=money,
-    )
-    print(ss.encode('windows-1250'))
-print('\r')
+    print(f'{dt};{cost};{addr};{desc};{iban};CEN{t_id};723')
