005a40cb7f45ececce16725c95f6912f150c8597
Marek Piechula <mpiechula@blees.co>
2020-08-14 16:21:15 +0200

    fix spa

--- spa/config/prod.env.js
+++ spa/config/prod.env.js
@@ -2,5 +2,5 @@ var process = require('process')
 
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"' + (process.env.URL_PREFIX || '') + '/api"',
+  API_URL: '"/' + (process.env.URL_PREFIX || '') + '/api"',
 }
