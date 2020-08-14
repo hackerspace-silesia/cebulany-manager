dd7452f488c631839274a2fa75d7c4a1cfd36019
Marek Piechula <mpiechula@blees.co>
2020-08-14 11:54:15 +0200

    cast to datetime for postgresql in sql_utils

--- cebulany/sql_utils.py
+++ cebulany/sql_utils.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func as sql_func
+from sqlalchemy import func as sql_func, cast, Date, DateTime
 
 from cebulany.models import db
 
@@ -6,17 +6,19 @@ from cebulany.models import db
 def get_year_month_col(column):
     database_type = db.engine.dialect.name
     if database_type == 'postgresql':
-        return sql_func.to_char(column, 'YYYY-MM')
+        dt_col = cast(column, DateTime)
+        return sql_func.to_char(dt_col, 'YYYY-MM')
     if database_type == 'sqlite':
         return sql_func.strftime('%Y-%m', column)
 
     raise AttributeError(f'Unknown database type: {database_type}')
 
 
-def get_year_col(column):
+def get_year_col(column: Date):
     database_type = db.engine.dialect.name
     if database_type == 'postgresql':
-        return sql_func.to_char(column, 'YYYY')
+        dt_col = cast(column, DateTime)
+        return sql_func.to_char(dt_col, 'YYYY')
     if database_type == 'sqlite':
         return sql_func.strftime('%Y', column)
 
