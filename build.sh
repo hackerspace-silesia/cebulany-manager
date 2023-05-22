ba0de66b19886abe36e19fc1890e1631cdc31283
Firemark <marpiechula@gmail.com>
2023-05-23 01:06:14 +0200

    fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 docker build . -f Dockerfile.backend -t cebulany-manager-backend
 docker build . -f Dockerfile.frontend -t cebulany-manager-frontend --build-arg URL_PREFIX=${URL_PREFIX:-}
 docker build . -t cebulany-manager
--- cebulany/app.py
+++ cebulany/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, safe_join
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_restful import Api
 
