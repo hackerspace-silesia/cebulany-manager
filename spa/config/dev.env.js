2a611496297578eaa147d6f8562da3c46e7a9ff5
Firemark <marpiechula@gmail.com>
2025-03-15 17:27:15 +0100

    Add dev mode for docker

--- spa/config/dev.env.js
+++ spa/config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge.merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://127.0.0.1:5000/api"'
+  API_URL: '"http://localhost:5000/api"'
 })
